2f07f964ef5c2d7e2541539edfeefccc17c0d54b
VieriElCrack1 <santosvieri9@gmail.com>
2024-04-28 17:53:24 -0500

    iniciando examen vieri

---
+++ src/main/java/com/daw1/cl1/DAW1_CL1_Grupo5/Daw1Cl1Grupo5Application.java
@@ -0,0 +1,13 @@
+package com.daw1.cl1.DAW1_CL1_Grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw1Cl1Grupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daw1Cl1Grupo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/daw1/cl1/DAW1_CL1_Grupo5/Daw1Cl1Grupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw1.cl1.DAW1_CL1_Grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daw1Cl1Grupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
